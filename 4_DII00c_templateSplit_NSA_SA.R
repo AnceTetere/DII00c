9465009fff37cc2b132c3492524ae488a7cdfad1
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-06-13 14:59:22 +0300

    Update 4_DII00c_templateSplit_NSA_SA.R

--- 4_DII00c_templateSplit_NSA_SA.R
+++ 4_DII00c_templateSplit_NSA_SA.R
@@ -1,21 +1,23 @@
-#1. Ielādē šablonu
-setwd(paste0(path, "\\izstrade\\intermediate_tables\\starting_templates"))
-load(paste0("template_DII00c_2000_", year, "Q", Q, ".RData"))
+path <- file.path(base_path, paste0(year, "Q", Q))
 
-s <- get(paste0("template_DII00c_2000_", year, "Q", Q)) 
+# 1. Ielādē šablonu
+template_path <- file.path(path, "izstrade", "intermediate_tables", "starting_templates")
+load(file.path(template_path, paste0("template_DII00c_2000_", year, "Q", Q, ".RData")))
+
+s <- get(paste0("template_DII00c_2000_", year, "Q", Q))
 rm(list = paste0("template_DII00c_2000_", year, "Q", Q))
 
-#2. Sadali NSA un SA apakštabulās
+# 2. Sadali NSA un SA apakštabulās
 s$SESON <- factor(s$SESON)
 
 s_split <- split(s, s$SESON)
 list2env(s_split, envir = .GlobalEnv)
 rm(s, s_split)
 
-#3. Saglabā apakštabulas
-setwd(paste0(path, "\\izstrade\\intermediate_tables"))
-save(NSA, file = "NSA.RData")
+# 3. Saglabā apakštabulas
+intermediate_path <- file.path(path, "izstrade", "intermediate_tables")
+save(NSA, file = file.path(intermediate_path, "NSA.RData"))
 rm(NSA)
 
-save(SA, file = "SA.RData")
+save(SA, file = file.path(intermediate_path, "SA.RData"))
 rm(SA)
